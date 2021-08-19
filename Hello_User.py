df9fa0178806342f194166077c445f3336696427
efernandez927 <esme.fernandez927@gmail.com>
2021-08-19 15:27:38 -0400

    two more classes

---
+++ Hello_User.py
@@ -0,0 +1,12 @@
+name = "Esme"
+print("Hello " + name + ".")
+name = "Meraz"
+print("Hello " + name + ".")
+name =input("What is your name?\n")
+print("Hello " + name + ".")
+
+
+
+
+
+
---
+++ Numbers1.py
@@ -0,0 +1,5 @@
+num1 = 5
+num2 = 12
+num3 = num1 + num2
+print("my number is " + str(num3))
+
